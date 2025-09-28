afb80f4184bbdb2ee15dd4cc45e3fc488eebec19
rupesh8690 <rupeshlove8690@gmail.com>
2025-09-29 01:20:40 +0530

    updated header

--- src/components/Header.js
+++ src/components/Header.js
@@ -78,7 +78,7 @@ const Header = () => {
                       <div className='text-3xl cursor-pointer relative flex justify-center' onClick={()=>setMenuDisplay(preve => !preve)}>
                         {
                           user?.profilePic ? (
-                            <img src={user?.profilePic} className='w-10 h-10 rounded-full' alt={user?.name} />
+                            <img src={user?.profilePic} className='w-10 h-10 rounded-full' />
                           ) : (
                             <FaRegCircleUser/>
                           )
