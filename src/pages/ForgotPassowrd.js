0d83c20b0c6d9d9cd5cd9527fd40668dc3f51993
rupesh8690 <rupeshlove8690@gmail.com>
2025-10-02 01:10:19 +0530

    crated the forgot password form

--- src/pages/ForgotPassowrd.js
+++ src/pages/ForgotPassowrd.js
@@ -1,11 +1,14 @@
 import React from 'react'
-
+import { useState } from 'react'
 const ForgotPassowrd = () => {
+  const [email,setEmail]=useState("");
+
+  
   return (
     <section id='forgot-password'>
       <div className='container mx-auto p-4'>
         <h1 className='text-3xl font-bold text-center mt-6'>Forgot Password</h1>
-        <form className='max-w-md mx-auto mt-6 p-6 border border-gray-300 rounded-lg shadow-md'>
+        <form className='pt-6 flex flex-col gap-2'>
           <div className='mb-4'>
             <label htmlFor='email' className='block text-gray-700 font-semibold mb-2'>
               Email Address
@@ -14,14 +17,16 @@ const ForgotPassowrd = () => {
               type='email'
               id='email'
               name='email'
-              className='w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500'
+              onChange={(e)=>setEmail(e.target.value)}
+              value={email}
+              className='w-full h-full outline-none bg-transparent'
               placeholder='Enter your email'
               required
             />
           </div>
           <button
             type='submit'
-            className='w-full bg-blue-500 text-white font-semibold py-2 px-4 rounded-lg hover:bg-blue-600 transition duration-300'
+            className=' bg-red-600 hover:bg-red-700 text-white px-6 py-2 w-full max-w-[150px] rounded-full hover:scale-110 transition-all mx-auto block mt-6'
           >
             Send Reset Link
           </button>
@@ -31,4 +36,4 @@ const ForgotPassowrd = () => {
   )
 }
 
-export default ForgotPassword;
+export default ForgotPassowrd;
